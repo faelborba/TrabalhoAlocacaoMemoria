13e87d3dce1e6f1c18c8d04d44f1fcacef663b00
faelborba <rafael.bds25@gmail.com>
2018-10-25 18:49:55 -0300

    subindo correção para o first-fit, algoritmo best-fit e worst-fit

--- best-fit.c
+++ best-fit.c
@@ -1,16 +1,16 @@
-/***Algoritmo de alocação de memória First-fit 
+/***Algoritmo de alocação de memória Best-fit 
 ****Desenvolvidor por Rafael Borba dos Santos
 ****Para A disciplina de Sistemas Operacionais da Ulbra
 ****/
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(){
-	int nblocos = 0, nprocessos = 0, i = 0, j = 0;
+	int nblocos = 0, nprocessos = 0, i = 0, j = 0, testa= 0, armazena = 0, posicao = 0;
 	int bloco[20], processo[20];
-	
-	printf("\nBem vindo ao First-fit");
+
+	printf("\nBem vindo ao Best-fit");
+
 	printf("\nDigite o número de blocos de memoria: "); 
 	scanf("%d", &nblocos);// definição da quantidade de blocos
 	for(i=0; i<nblocos; i++){// nesse trecho, definimos o tamanho de memória que cada bolco irá possuir.
@@ -23,16 +23,33 @@ int main(){
 		printf("Digite o tamanho do processo %d \n", i);
 		scanf("%d", &processo[i]);
 	}
-	
+	armazena = -1;
 	for(j=0; j<nprocessos; j++){
 		for(i=0; i<nblocos; i++){//for dos blocos
-			if(processo[j]<=bloco[i]){
+			if(processo[j]==bloco[i]){// se o processo for do tamanho do bloco já armazena e segue o fluxo para testar o próximo processo
 				printf("O processo %d foi alocado no bloco %d \n",j,i);
-				bloco[i] = bloco[i]-processo[j];
+				armazena = -1;
+				bloco[i] = 0;
 				processo[j]=100000;
-				
 				break;
 			}
+
+			if(processo[j] < bloco[i]){
+				if(armazena < processo[j]){
+					//printf("##Entreiii\n");
+					armazena = processo[j];
+					posicao = i;
+				}
+				
+			}
+			if((i == nblocos - 1) && armazena != -1){// quando chegamos ao final do vetor de blocos alocamos na melhor posição encontrada.
+				printf("O processo %d foi alocado no bloco %d \n",j,posicao);
+				armazena = -1;
+				bloco[posicao] = bloco[posicao]-processo[j];
+				processo[j]=100000;
+				break;
+			}
+			
 		}		
 	}
 
--- worst-fit.c
+++ worst-fit.c
@@ -1,4 +1,4 @@
-/***Algoritmo de alocação de memória First-fit 
+/***Algoritmo de alocação de memória Worst-fit 
 ****Desenvolvidor por Rafael Borba dos Santos
 ****Para A disciplina de Sistemas Operacionais da Ulbra
 ****/
@@ -7,10 +7,10 @@
 #include <stdlib.h>
 
 int main(){
-	int nblocos = 0, nprocessos = 0, i = 0, j = 0;
+	int nblocos = 0, nprocessos = 0, i = 0, j = 0, armazena = 0, posicao = 0;
 	int bloco[20], processo[20];
-	
-	printf("\nBem vindo ao First-fit");
+ 	
+ 	printf("\nBem vindo ao Worst-fit"); 
 	printf("\nDigite o número de blocos de memoria: "); 
 	scanf("%d", &nblocos);// definição da quantidade de blocos
 	for(i=0; i<nblocos; i++){// nesse trecho, definimos o tamanho de memória que cada bolco irá possuir.
@@ -23,16 +23,25 @@ int main(){
 		printf("Digite o tamanho do processo %d \n", i);
 		scanf("%d", &processo[i]);
 	}
-	
+	armazena = 100000;
 	for(j=0; j<nprocessos; j++){
 		for(i=0; i<nblocos; i++){//for dos blocos
-			if(processo[j]<=bloco[i]){
-				printf("O processo %d foi alocado no bloco %d \n",j,i);
-				bloco[i] = bloco[i]-processo[j];
-				processo[j]=100000;
+
+			if(processo[j] <= bloco[i]){
+				if(armazena > processo[j]){//se o armazena for maior que o processo armazena recebe o processo 
+					armazena = processo[j];
+					posicao = i;
+				}
 				
+			}
+			if((i == nblocos - 1)){// quando chegamos ao final do vetor de blocos alocamos na melhor posição encontrada.
+				printf("O processo %d foi alocado no bloco %d \n",j,posicao);
+				armazena = -1;
+				bloco[posicao] = bloco[posicao]-processo[j];
+				processo[j]=100000;
 				break;
 			}
+			
 		}		
 	}
 
