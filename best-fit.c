1859a09021d4b6fc094303bd05b90122557090a4
faelborba <rafael.bds25@gmail.com>
2018-10-26 00:31:50 -0300

    finalizando o trabalho, ajustes em todos os algoritmos

--- best-fit.c
+++ best-fit.c
@@ -6,16 +6,16 @@
 #include <stdlib.h>
 
 int main(){
-	int nblocos = 0, nprocessos = 0, i = 0, j = 0, testa= 0, armazena = 0, posicao = 0;
-	int bloco[20], processo[20];
+	int nlacunas = 0, nprocessos = 0, i = 0, j = 0, k=0, testa= 0, armazena = 0, posicao = 0;
+	int lacuna[20], processo[20];
 
 	printf("\nBem vindo ao Best-fit");
 
-	printf("\nDigite o número de blocos de memoria: "); 
-	scanf("%d", &nblocos);// definição da quantidade de blocos
-	for(i=0; i<nblocos; i++){// nesse trecho, definimos o tamanho de memória que cada bolco irá possuir.
-		printf("\nDigite o tamanho do bloco %d: ", i);
-		scanf("%d", &bloco[i]);
+	printf("\nDigite o número de lacunas de memoria: "); 
+	scanf("%d", &nlacunas);// definição da quantidade de lacunas
+	for(i=0; i<nlacunas; i++){// nesse trecho, definimos o tamanho de memória que cada bolco irá possuir.
+		printf("\nDigite o tamanho do lacuna %d: ", i);
+		scanf("%d", &lacuna[i]);
 	}
 	printf("Digite a quantidade de processos: \n"); 
 	scanf("%d", &nprocessos);
@@ -23,39 +23,66 @@ int main(){
 		printf("Digite o tamanho do processo %d \n", i);
 		scanf("%d", &processo[i]);
 	}
-	armazena = -1;
+	printf("\nDados de entrada");
+	printf("\nLacunas: ");
+	for(k=0; k<nlacunas; k++){
+		if(k + 1 == nlacunas ){
+			printf("e %dK ",lacuna[k]);
+		}else if(k + 2 == nlacunas ){
+			printf("%dK ",lacuna[k]);
+		}else{
+			printf("%dK, ",lacuna[k]);
+		}
+	}
+	printf("\nProcessos: ");
+	for(k=0; k<nprocessos; k++){
+		if(k + 1 == nprocessos ){
+			printf("e %dK ",processo[k]);
+		}else if(k + 2 == nprocessos ){
+			printf("%dK ",processo[k]);
+		}else{
+			printf("%dK, ",processo[k]);
+		}
+	}
+	armazena = 100000;
 	for(j=0; j<nprocessos; j++){
-		for(i=0; i<nblocos; i++){//for dos blocos
-			if(processo[j]==bloco[i]){// se o processo for do tamanho do bloco já armazena e segue o fluxo para testar o próximo processo
-				printf("O processo %d foi alocado no bloco %d \n",j,i);
-				armazena = -1;
-				bloco[i] = 0;
+		for(i=0; i<nlacunas; i++){//for dos lacunas
+			if(processo[j]==lacuna[i]){// se o processo for do tamanho do lacuna já armazena e segue o fluxo para testar o próximo processo
+				printf("\n O processo %d foi alocado no lacuna %d \t: ",j,i);
+				armazena = 100000;
+				lacuna[i] = 0;
 				processo[j]=100000;
 				break;
 			}
 
-			if(processo[j] < bloco[i]){
-				if(armazena < processo[j]){
-					//printf("##Entreiii\n");
-					armazena = processo[j];
+			if(processo[j] < lacuna[i]){
+				if(armazena > lacuna[i] - processo[j]){
+					armazena = lacuna[i] - processo[j];
 					posicao = i;
 				}
 				
 			}
-			if((i == nblocos - 1) && armazena != -1){// quando chegamos ao final do vetor de blocos alocamos na melhor posição encontrada.
-				printf("O processo %d foi alocado no bloco %d \n",j,posicao);
-				armazena = -1;
-				bloco[posicao] = bloco[posicao]-processo[j];
+			if((i == nlacunas - 1) && armazena != 100000){// quando chegamos ao final do vetor de lacunas alocamos na melhor posição encontrada.
+				printf("\n O processo %d foi alocado no lacuna %d \t: ",j,posicao);
+				armazena = 100000;
+				lacuna[posicao] = lacuna[posicao]-processo[j];
 				processo[j]=100000;
 				break;
 			}
 			
-		}		
-	}
-
-	for(i=0; i<nprocessos; i++){
-		if(processo[i]!=100000){
-			printf("Processo %d não foi alocado\n",i);
 		}
+		if(processo[j]!=100000){
+			printf("\n O Processo %d não foi alocado \t\t: ",j);
+		}
+		for(k=0; k<nlacunas; k++){
+			if(k + 1 == nlacunas ){
+				printf("e %dK ",lacuna[k]);
+			}else if(k + 2 == nlacunas ){
+				printf("%dK ",lacuna[k]);
+			}else{
+				printf("%dK, ",lacuna[k]);
+			}
+		}			
 	}
+	printf("\n\n");
 }
--- first-fit.c
+++ first-fit.c
@@ -7,15 +7,15 @@
 #include <stdlib.h>
 
 int main(){
-	int nblocos = 0, nprocessos = 0, i = 0, j = 0;
-	int bloco[20], processo[20];
+	int nlacunas = 0, nprocessos = 0, i = 0, j = 0, k = 0;
+	int lacuna[20], processo[20];
 	
 	printf("\nBem vindo ao First-fit");
-	printf("\nDigite o número de blocos de memoria: "); 
-	scanf("%d", &nblocos);// definição da quantidade de blocos
-	for(i=0; i<nblocos; i++){// nesse trecho, definimos o tamanho de memória que cada bolco irá possuir.
-		printf("\nDigite o tamanho do bloco %d: ", i);
-		scanf("%d", &bloco[i]);
+	printf("\nDigite o número de lacunas de memoria: "); 
+	scanf("%d", &nlacunas);// definição da quantidade de lacunas
+	for(i=0; i<nlacunas; i++){// nesse trecho, definimos o tamanho de memória que cada bolco irá possuir.
+		printf("\nDigite o tamanho do lacuna %d: ", i);
+		scanf("%d", &lacuna[i]);
 	}
 	printf("Digite a quantidade de processos: \n"); 
 	scanf("%d", &nprocessos);
@@ -23,22 +23,55 @@ int main(){
 		printf("Digite o tamanho do processo %d \n", i);
 		scanf("%d", &processo[i]);
 	}
+	printf("\nDados de entrada");
+	printf("\nLacunas: ");
+	for(k=0; k<nlacunas; k++){
+		if(k + 1 == nlacunas ){
+			printf("e %dK ",lacuna[k]);
+		}else if(k + 2 == nlacunas ){
+			printf("%dK ",lacuna[k]);
+		}else{
+			printf("%dK, ",lacuna[k]);
+		}
+	}
+	printf("\nProcessos: ");
+	for(k=0; k<nprocessos; k++){
+		if(k + 1 == nprocessos ){
+			printf("e %dK ",processo[k]);
+		}else if(k + 2 == nprocessos ){
+			printf("%dK ",processo[k]);
+		}else{
+			printf("%dK, ",processo[k]);
+		}
+	}
 	
 	for(j=0; j<nprocessos; j++){
-		for(i=0; i<nblocos; i++){//for dos blocos
-			if(processo[j]<=bloco[i]){
-				printf("O processo %d foi alocado no bloco %d \n",j,i);
-				bloco[i] = bloco[i]-processo[j];
+		for(i=0; i<nlacunas; i++){//for dos lacunas
+			if(processo[j]<=lacuna[i]){
+				printf("\n O processo %d foi alocado no lacuna %d \t: ",j,i);
+				lacuna[i] = lacuna[i]-processo[j];
 				processo[j]=100000;
 				
 				break;
 			}
-		}		
+		}
+		if(processo[j]!=100000){
+			printf("\n O Processo %d não foi alocado \t\t: ",j);
+		}
+		for(k=0; k<nlacunas; k++){
+			if(k + 1 == nlacunas ){
+				printf("e %dK ",lacuna[k]);
+			}else if(k + 2 == nlacunas ){
+				printf("%dK ",lacuna[k]);
+			}else{
+				printf("%dK, ",lacuna[k]);
+			}
+		}			
 	}
-
-	for(i=0; i<nprocessos; i++){
+	printf("\n\n");
+	/*for(i=0; i<nprocessos; i++){
 		if(processo[i]!=100000){
 			printf("Processo %d não foi alocado\n",i);
 		}
-	}
+	}*/
 }
